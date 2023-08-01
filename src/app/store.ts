764eec1db42f5077a86700673be502a66eb6b667
Jurilica <80757501+Jurilica@users.noreply.github.com>
2023-08-01 21:41:34 +0200

    Connected to backend

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { problemApiSlice } from '../features/problems/problem-api-slice';
+
 
 export const store = configureStore({
     reducer: {
-
+        [problemApiSlice.reducerPath]: problemApiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(problemApiSlice.middleware)
     }
 });
 
--- src/features/problems/problem-api-slice.ts
+++ src/features/problems/problem-api-slice.ts
@@ -2,25 +2,22 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const API_URL = "https://localhost:44395/";
 
-interface CreateProblemRequest {
-    title: string;
-    description: string;
-}
-
-interface CreateProblemResponse {
-    id: number;
-}
-
-const problemApiSlice = createApi({
+export const problemApiSlice = createApi({
     reducerPath: 'api',
     baseQuery : fetchBaseQuery({
         baseUrl: API_URL
     }),
     endpoints(builder) {
         return {
+            getProblems: builder.query<GetProblemsResponse, void>({
+                query: () => ({
+                    url: "admin/problems",
+                    method: "GET"
+                })
+            }),
             addProblem: builder.mutation<CreateProblemResponse, CreateProblemRequest>({
                 query: (problem) => ({
-                    url: "/admin/problems",
+                    url: "admin/problems",
                     method: "POST",
                     body: problem
                 })
@@ -28,3 +25,45 @@ const problemApiSlice = createApi({
         }
     }
 });
+
+export const {useAddProblemMutation, useGetProblemsQuery} = problemApiSlice;
+
+export interface CreateProblemRequest {
+    title: string;
+    description: string;
+}
+
+export interface CreateProblemResponse {
+    id: number;
+}
+
+export interface GetProblemsResponse {
+    problems: GetProblemsData[];
+}
+
+export interface GetProblemsData {
+    id: number;
+    title: string;
+    description: string;
+    problemStatus: ProblemStatus;
+    examples: ExampleData[];
+    testCases: TestCaseData[];
+}
+
+export interface ExampleData {
+    id: number;
+    input: string;
+    output: string;
+    explanation: string;
+}
+
+export interface TestCaseData {
+    id: number;
+    input: string;
+    output: string;
+}
+
+export enum ProblemStatus {
+    Published = "Published",
+    Unpublished = "Unpublished"
+}
